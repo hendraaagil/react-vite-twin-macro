d36d975b90d52c323af80bc592d9f1b98fea5731
hendraaagil <hendraaagil@gmail.com>
2021-08-29 19:22:59 +0700

    styles: add & change assets

--- src/App.jsx
+++ src/App.jsx
@@ -1,63 +1,75 @@
 import 'twin.macro';
-import { useState } from 'react';
+import { Fragment, useState } from 'react';
 
-import logo from './logo.svg';
+import reactLogo from './assets/react-logo.png';
+import twinLogo from './assets/twin-logo.png';
+import viteLogo from './assets/vite-logo.svg';
 import CustomLink from './components/CustomLink';
 import CustomButton from './components/CustomButton';
 
 function App() {
   const [count, setCount] = useState(0);
+  const logos = [
+    { src: reactLogo, alt: 'React logo' },
+    { src: viteLogo, alt: 'Vite logo' },
+    { src: twinLogo, alt: 'Twin macro logo' },
+  ];
 
   return (
-    <div tw="text-center">
-      <header tw="flex flex-col items-center justify-center min-h-screen bg-[#282c34] text-gray-50 text-lg">
-        <img
-          src={logo}
-          alt="React logo"
-          tw="h-[40vmin] pointer-events-none motion-safe:animate-spin-slow"
-        />
-        <p>Hello React + Vite + twin.macro!</p>
-        <div tw="flex items-center mt-4">
-          <CustomButton
-            clickAction={() => setCount((count) => count - 1)}
-            color="red"
-          >
-            -
-          </CustomButton>
-          <p tw="mx-4">{count}</p>
-          <CustomButton
-            clickAction={() => setCount((count) => count + 1)}
-            color="blue"
-          >
-            +
-          </CustomButton>
-        </div>
+    <main tw="flex flex-col items-center justify-center min-h-screen bg-[#282c34] text-gray-50 text-lg text-center">
+      <div tw="flex items-center mb-4">
+        {logos.map((logo, index) => (
+          <Fragment key={logo.alt}>
+            <img
+              src={logo.src}
+              alt={logo.alt}
+              tw="mx-4 h-[20vmin] pointer-events-none"
+            />
+            {index + 1 !== logos.length && <p tw="text-4xl">+</p>}
+          </Fragment>
+        ))}
+      </div>
+      <p>Hello React + Vite + twin.macro!</p>
+      <div tw="flex items-center mt-4">
         <CustomButton
-          clickAction={() => setCount(0)}
-          color="emerald"
-          padding="wide"
-          type="reset"
+          clickAction={() => setCount((count) => count - 1)}
+          color="red"
         >
-          Reset
+          -
         </CustomButton>
-        <p tw="mt-4">
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <div tw="flex">
-          <CustomLink text="Learn React" url="https://reactjs.org" />
-          {' | '}
-          <CustomLink
-            text="Vite Docs"
-            url="https://vitejs.dev/guide/features.html"
-          />
-          {' | '}
-          <CustomLink
-            text="twin.macro"
-            url="https://github.com/ben-rogerson/twin.macro"
-          />
-        </div>
-      </header>
-    </div>
+        <p tw="mx-4">{count}</p>
+        <CustomButton
+          clickAction={() => setCount((count) => count + 1)}
+          color="blue"
+        >
+          +
+        </CustomButton>
+      </div>
+      <CustomButton
+        clickAction={() => setCount(0)}
+        color="emerald"
+        padding="wide"
+        type="reset"
+      >
+        Reset
+      </CustomButton>
+      <p tw="mt-4">
+        Edit <code>App.jsx</code> and save to test HMR updates.
+      </p>
+      <div tw="flex">
+        <CustomLink text="Learn React" url="https://reactjs.org" />
+        {' | '}
+        <CustomLink
+          text="Vite Docs"
+          url="https://vitejs.dev/guide/features.html"
+        />
+        {' | '}
+        <CustomLink
+          text="twin.macro"
+          url="https://github.com/ben-rogerson/twin.macro"
+        />
+      </div>
+    </main>
   );
 }
 
